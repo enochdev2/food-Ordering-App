81c535c8fa124c67d83ab62c8e82805b5af7026f
enoch-promise <enochpromise.va@gmail.com>
2023-12-05 13:05:57 -0800

    the begining part

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#f13a01',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
